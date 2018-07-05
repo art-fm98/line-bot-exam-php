05badbfe1b03ce9fd1db779184add624dd335199
art-fm98 <40850614+art-fm98@users.noreply.github.com>
2018-07-05 15:09:49 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '0V/NBfUfzghirthKZ5UZfeSjGY3nUJHwxnEytQY7xgPkicaLM4NVYI0t5H+zU/1V2uWa78W12A0BafcwDagfATUhkfEo0vFnvoPkNBmJKlILtdix5r1ERUf3z8Y2oSqKqqF7EzFVdxbywvis/8kQWAdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
